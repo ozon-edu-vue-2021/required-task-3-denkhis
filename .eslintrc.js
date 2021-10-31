9f7e79775e7e046537c7da409276d034ae816625
Denis <dnkhisamiev@gmail.com>
2021-10-31 22:25:30 +0300

    Updated code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     parser: "babel-eslint",
   },
   rules: {
+    "indent": ["error", 2],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
